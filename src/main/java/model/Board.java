ba3eecc835d9705e01a71af7447ff6a517b9d790
sinaqane <sina.qane@yahoo.com>
2021-06-20 15:33:12 +0430

    Added setShip method to Board and re-built project

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -2,6 +2,8 @@ package model;
 
 import model.cell.Cell;
 
+import java.util.List;
+
 public class Board
 {
     private final Cell[][] board = new Cell[10][10];
@@ -15,6 +17,16 @@ public class Board
                 board[i][j] = new Cell(i, j);
             }
         }
+    }
+
+    public void setShip(List<Integer[]> coordinates)
+    {
+        Cell[] cells = new Cell[coordinates.size()];
+        for (int i = 0; i < coordinates.size(); i++)
+        {
+            cells[i] = board[coordinates.get(i)[0]][coordinates.get(i)[1]];
+        }
+        new Ship(cells);
     }
 
     public Cell getCell(int x, int y)
